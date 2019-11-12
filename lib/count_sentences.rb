6f7a33e87e56b2c5c3fab865694296c90ed53eff
Andrew H <andrew.o.hibbard@gmail.com>
2019-11-12 20:37:47 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    #string.match(/[.]$/) ? true : false
+    if self.end_with?(".")
+      true
+    else 
+      false
+    end 
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else 
+      false 
+    end 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else 
+      false
+    end 
   end
 
   def count_sentences
-
+        arr = self.split(/[.?!]/)
+        arr.delete_if{|sentence| sentence.empty?}
+        arr.length
   end
+  
 end
