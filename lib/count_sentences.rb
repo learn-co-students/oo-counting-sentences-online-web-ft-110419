4359d6c6cf84e6282568ba23d4d7f1374331325c
Ken Goad <goadke@g.cofc.edu>
2019-11-14 21:22:42 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,32 @@
 require 'pry'
 
 class String
+  def string_self
+   self
+  end  
 
   def sentence?
-    
+     if string_self.end_with?(".")
+        return true
+      else return false
+    end
   end
 
   def question?
-
+     if string_self.end_with?("?")
+        return true
+      else return false
+    end
   end
 
   def exclamation?
-
+if string_self.end_with?("!")
+        return true
+      else return false
+    end
   end
 
   def count_sentences
-
+     string_self.squeeze('.!?').count('.!?')
   end
 end
